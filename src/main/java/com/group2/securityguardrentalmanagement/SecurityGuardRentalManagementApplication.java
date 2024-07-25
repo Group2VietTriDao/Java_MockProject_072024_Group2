fca481b28738afae0fdbdce3f10d0553c86f9481
Lehuyhuynh <113335129+lehuyhuynh@users.noreply.github.com>
2024-07-25 20:28:42 +0700

    Task Discuss and Unified Framework

---
+++ src/main/java/com/group2/securityguardrentalmanagement/SecurityGuardRentalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.group2.securityguardrentalmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityGuardRentalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityGuardRentalManagementApplication.class, args);
+	}
+
+}
